fd1abca40eda91e46c55af7772c0f80cb83b95c9
PriyanshuV <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-08-05 16:43:33 +0530

    add methods Student.java !

---
+++ src/Class/Student.java
@@ -0,0 +1,20 @@
+/*
+ *   Created by IntelliJ IDEA.
+ *   User: Priyanshu
+ *   Date: 05/08/20
+ *   Time: 04:25 PM
+ */
+
+package Class;
+
+public class Student {
+    int studentRollNumber;
+    String studentName;
+    float studentMarks;
+
+    void study() {
+    }
+
+    void takeExam() {
+    }
+}
