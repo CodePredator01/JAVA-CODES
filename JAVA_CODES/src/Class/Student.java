0fa14e92e6b8ca2caaf1e7cc312fe5deb22c4451
CodePredator01 <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-08-05 23:00:03 +0530

    Polymorphism !

--- JAVA_CODES/src/Class/Student.java
+++ JAVA_CODES/src/Class/Student.java
@@ -2,7 +2,7 @@
  *   Created by IntelliJ IDEA.
  *   User: Priyanshu
  *   Date: 05/08/20
- *   Time: 04:25 PM
+ *   Time: 11:25 PM
  */
 
 package Class;
---
+++ JAVA_CODES/src/Class/Zoo.java
@@ -0,0 +1,7 @@
+package Class;
+
+class Zoo {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+    }
+}
