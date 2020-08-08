8186d0affdaee73189a75cffc577080af273146e
CodePredator01 <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-08-09 01:51:48 +0530

    create (methods) a java class for store car, bicycle details !

---
+++ JAVA_CODES/src/Methods/Bicycle.java
@@ -0,0 +1,31 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: CodePredator01
+ * Date: 09-08-2020
+ * Time: 01:48 AM
+ * */
+
+package Methods;
+
+class Bicycle {
+    private String name;
+    private String color;
+    private int distance = 0;
+
+    Bicycle(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public void printData() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Color: " + this.color);
+        System.out.println("Distance: " + this.distance + "km");
+    }
+
+    public void run(int distance) {
+        System.out.println("Moving " + distance + "km ...");
+        this.distance += distance;
+        System.out.println("Distance: " + this.distance + "km");
+    }
+}
