8a198ab66c7f97dd29ebf216a01726c5d570418f
CodePredator01 <priyanshu.vishwakarma_cs19@gla.ac.in>
2020-08-10 08:44:22 +0530

    add a java class for convert number to words form !

---
+++ JAVA_CODES/src/LabTask/NumberConvertToWords.java
@@ -0,0 +1,13 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: CodePredator01
+ * Date: 10-08-2020
+ * Time: 08:43 AM
+ * File: NumberConvertToWords.java
+ * */ // 1. author information
+
+// 2. package information
+package LabTask;
+
+public class NumberConvertToWords {
+}
